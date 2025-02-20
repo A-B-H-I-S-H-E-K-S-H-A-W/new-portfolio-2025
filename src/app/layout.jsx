551cb348d9356bb11b88ec4cb2fb7925c90e0f5b
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-02-20 20:21:15 +0530

    Changed the website meta description

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,8 @@ import icon from "@/jsm-logo.png";
 
 export const metadata = {
   title: "Abhishek's Portfolio",
-  description: "Modern Portfolio",
+  description:
+    "Hello! I'm Abhishek Shaw, a passionate and dedicated Full Stack Developer with a strong foundation in both front-end and back-end technologies. With extensive experience in creating dynamic, high-performance web applications, I thrive on turning complex problems into elegant, user-friendly solutions.",
 };
 
 export default function RootLayout({ children }) {
