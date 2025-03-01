4bba393ebdd2d8911832e46685b648febfe34894
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-03-01 19:35:06 +0530

    Added keyword for SEO

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,6 +5,8 @@ import icon from "@/jsm-logo.png";
 
 export const metadata = {
   title: "Abhishek Shaw - Full Stack Web Developer Portfolio",
+  keywords:
+    "full stack developer, web development portfolio, Abhishek Shaw, front-end, back-end, web, website, contact",
   description:
     "Explore the diverse portfolio of Abhishek Shaw, a skilled full stack web developer. Discover his creative and user-friendly solutions across front-end and back-end technologies.",
 };
